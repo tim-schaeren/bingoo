c42fa1213588d2fa2b51a4fc07b9673df7f77bd6
tim-schaeren <tim.schaeren@gmail.com>
2026-03-22 22:50:24 +0100

    chore: bump iOS build number to 6

--- app.config.js
+++ app.config.js
@@ -15,7 +15,7 @@ export default {
     ios: {
       supportsTablet: false,
       bundleIdentifier: 'com.bingoo.app',
-      buildNumber: '5',
+      buildNumber: '6',
       infoPlist: {
         ITSAppUsesNonExemptEncryption: false,
         NSUserNotificationsUsageDescription: 'bingoo notifies you when a prediction is marked, the game starts, or someone wins.',
